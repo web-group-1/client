a38155dd79ae90a2d9696c3127b0591043a6b24a
Biniyam Seid <Biniyamseid3@gmail.com>
2023-02-03 17:54:55 +0300

    clean bugs

--- scripts/register.js
+++ scripts/register.js
@@ -1,14 +1,8 @@
 document.getElementById("register").addEventListener('submit',Registration);
 async function Registration(e){
-    
-//The preventDefault() method cancels the event if it is cancelable, meaning that the default action that belongs to the event will not occur.
-//For example, this can be useful when:
-//Clicking on a "Submit" button, prevent it from submitting a form
     e.preventDefault()
     let firstname = document.getElementById("form4Example0").value;
     let lastname = document.getElementById("form4Example4").value;
-    // var schools = document.getElementById("school");
-    // var school = schools.value;
     let password = document.getElementById("form4Example3").value;
     let email = document.getElementById("form4Example1").value;
     const response = await fetch('http://localhost:3000/users',{
