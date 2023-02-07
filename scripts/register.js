beacdd673d0d9639ce81b78ca439e7af0bccfdd2
Biniyam Seid <Biniyamseid3@gmail.com>
2023-02-07 03:09:16 +0300

    add a popup window for the alert messages

--- scripts/register.js
+++ scripts/register.js
@@ -5,26 +5,50 @@ async function Registration(e){
     let lastname = document.getElementById("form4Example4").value;
     let password = document.getElementById("form4Example3").value;
     let email = document.getElementById("form4Example1").value;
-    const response = await fetch('http://localhost:3000/users',{
-        method:'POST',
-        headers: {
-            'Accept':'Application/json,text/plain,*/*',
-            'content-type':'application/json'
-        },
-        body:JSON.stringify({
-            firstName:firstname,
-            lastName:lastname,
-            email:email,
-            password:password
+    try {
+
+        const response = await fetch('http://localhost:3000/users',{
+            method:'POST',
+            headers: {
+                'Accept':'Application/json,text/plain,*/*',
+                'content-type':'application/json'
+            },
+            body:JSON.stringify({
+                firstName:firstname,
+                lastName:lastname,
+                email:email,
+                password:password
+            })
         })
-    })
-    .then((res)=> {
-        console.log(res.status)
-        if (res.status === '201'|| 201) {
-        alert('User created successfully');
-        window.setTimeout(() => {
-            location.assign('../index.html');
-        }, 700)}
-        });
+        .then((res)=> {
+            console.log(res.status)
+            if (res.status === '201'|| 201) {
+                Swal.fire(
+                    'Good job!',
+                    'You clicked the button!',
+                    'success'
+                  );
+            window.setTimeout(() => {
+                location.assign('../index.html');
+            }, 700)}
+            else{
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'please try again!',
+                
+                  })
+            }
+            });
+        
+    } catch (error) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'please try again!'
+          })
+        
+    }
+   
 }
 
